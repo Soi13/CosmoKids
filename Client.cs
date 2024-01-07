e91c3a695bd092912161bce578752e06af7ea28a
Oleg Skvortsov <highsoft.soi@gmail.com>
2024-01-06 19:44:52 -0800

    Update Client.cs

--- Client.cs
+++ Client.cs
@@ -5,9 +5,9 @@ namespace CosmoKids
     abstract class Client
     {
         private const string provider_name = "COSMOKIDS ACADEMY LLC";
-        private const string provider_address = "1984 Bernice Way, San Jose, CA, 95124";
-        private const string provider_phone = "+1 (408) 667 2985";
-        private const string provider_email = "cosmokids.academy@gmail.com";
+        private const string provider_address = "";
+        private const string provider_phone = "";
+        private const string provider_email = "";
         private string client_surname;
         private string client_name;
         private string client_date_birth;
